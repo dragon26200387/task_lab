f30de91b8af7713aff102c4ceb479876f4e6f85c
pylearn2019 <dsfgh@gdsf.sd>
2019-06-26 17:55:25 +0300

    Start

---
+++ labirint.py
@@ -0,0 +1,17 @@
+import turtle 
+
+wn = turtle.Screen()
+#wn.bgpic('labirint.gif')
+mouse = turtle.Turtle()
+mouse.up()
+mouse.color('red')
+mouse.width(5)
+mouse.goto(-300, 30)
+
+mouse.down()
+mouse.fd(90)
+
+wn.exitonclick()
+
+
+
